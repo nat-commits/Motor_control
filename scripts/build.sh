f6ee39f0afd35d2efddf99edec297b8431a5483e
nat-commits <teacher1538@gmail.com>
2026-05-28 15:16:49 +0200

    Add files via upload

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+set -e
 
+IMAGE_NAME="robot_control_system"
+IMAGE_TAG="latest"
+
+echo "=== Запуск сборки через BuildKit с полной очисткой кэша ==="
+
+# Включаем BuildKit и собираем образ с полной очисткой кэша пакетов
+DOCKER_BUILDKIT=1 docker build --no-cache -t ${IMAGE_NAME}:${IMAGE_TAG} -f docker/Dockerfile .
+
+echo "=== Сборка успешно завершена! ==="
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+set -e
+xhost +local:docker > /dev/null
+echo "=== Запуск Docker-контейнера ==="
+docker run -it --rm \
+    --name robot_control_container \
+    --net=host \
+    --ipc=host \
+    --pid=host \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="$(pwd)/src:/robot_app" \
+    robot_control_system:latest
+xhost -local:docker > /dev/null
